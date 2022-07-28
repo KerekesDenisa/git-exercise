da4a7770f00642fc1a4c107cdc6eebbe9cd151a8
Kerekes Denisa <denisa.camelia0717@gmail.com>
2022-07-28 10:49:32 +0300

    commit all

--- testing9/Program.cs
+++ testing9/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello Deni, it's your first day of automation testing at Evozon!");
